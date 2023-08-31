cd2dd987fd02da2eb7ea58aa9470c682636306ac
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-08-31 21:36:05 +0530

    create generic dao class

---
+++ src/main/java/com/bookstore/dao/GenericDao.java
@@ -0,0 +1,14 @@
+package com.bookstore.dao;
+
+import java.util.List;
+
+interface GenericDao<T> {
+	
+	public T create(T t);
+	public T update(T t) ;
+	public void delete(Object id);
+	public T get(Object id);
+	public List<T> listAll();
+	public long count();
+	
+}
