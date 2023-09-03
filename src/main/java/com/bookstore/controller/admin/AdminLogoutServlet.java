98283f25b8edb879e8078240b6f27e1bf4280c00
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-09-03 18:32:29 +0530

    admin logout functionality-1

---
+++ src/main/java/com/bookstore/controller/admin/AdminLogoutServlet.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookstore.controller.admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/admin/logout"})
+public class AdminLogoutServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            HttpSession session=request.getSession();
+            request.removeAttribute("userEmail");
+            
+            RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+            rd.forward(request, response);
+        }
+    }
+}
