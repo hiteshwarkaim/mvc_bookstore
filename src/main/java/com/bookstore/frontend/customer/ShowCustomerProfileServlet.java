fd47eeabe5e0730c9b9598b6546eefb38f39855c
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-09-11 22:14:51 +0530

    frontend --> customer login filter

--- src/main/java/com/bookstore/frontend/customer/ShowCustomerProfileServlet.java
+++ src/main/java/com/bookstore/frontend/customer/ShowCustomerProfileServlet.java
@@ -25,7 +25,6 @@ public class ShowCustomerProfileServlet extends HttpServlet {
             
            CustomerService service=new CustomerService(request, response);
            service.showCustomerProfile();
-
         }
     }
 }
