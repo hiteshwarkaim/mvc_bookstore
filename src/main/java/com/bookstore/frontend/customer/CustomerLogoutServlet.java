a57cc2b01b446a0db912aefd5db669ae0a2c8e37
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-09-11 21:56:16 +0530

    frontend --> logout customer

---
+++ src/main/java/com/bookstore/frontend/customer/CustomerLogoutServlet.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookstore.frontend.customer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "CustomerLogoutServlet", urlPatterns = {"/logout"})
+public class CustomerLogoutServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            request.getSession().removeAttribute("loggedCustomer");
+            
+//            RequestDispatcher requestDispatcher = request.getRequestDispatcher("frontend/index.jsp");
+//            requestDispatcher.forward(request, response);
+            response.sendRedirect(request.getContextPath());
+            
+        }
+    }
+}
